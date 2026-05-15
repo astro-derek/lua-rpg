87ca2aba094f87848be2f01135e35e2c16f8ba83
astro-derek <dpmn83@gmail.com>
2026-05-15 20:08:05 +0100

    added version and description

--- src/main.lua
+++ src/main.lua
@@ -1,1 +1,8 @@
-print "hello world"
+local module = {}
+
+module.version = "0.0.1"
+module.author = "Derek Potter"
+module.license = "MIT"
+module.description = "An RPG Engine built in lua."
+
+return module
